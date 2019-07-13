9e3fbb466c6098eab77640cf09a7b599e3f03434
chenshenhai <cshenhai@gmail.com>
2019-07-13 23:15:25 +0800

    feat: add demo

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
-const name = 'pictool';
-const str = `hello world! ${name}`;
-console.log(str);
+import Pictool from 'pictool';
+
+
+var src = './image/lena.jpg';
+var Sandbox = Pictool.browser.Sandbox;
+var sandbox = new Sandbox(src);
+var dom = document.querySelector('#display');
+
+sandbox.queueProcess([
+  {
+    process: 'sobel',
+    options: {},
+  },
+  {
+    process: 'invert',
+    options: {},
+  }
+]).then(function(base64) {
+  dom.innerHTML = `<img src="${base64}" />`;
+}).catch(function(err) {
+  console.log(err);
+});
+
+console.log(Pictool);
